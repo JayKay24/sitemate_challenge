897b81baed7e6d047ad4e8c6ff325adc35bcac29
nkinyua-mufin <n.kinyua@mufin.co.kr>
2024-09-07 12:02:41 +0300

    import & utilize service class in endpoints
    Signed-off-by: nkinyua-mufin <n.kinyua@mufin.co.kr>

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const app = express();
+const Service = require('./service');
+
+const service = new Service();
 
 app.get('/issues/:id', function (req, res) {
-  res.send('Hello World');
+  try {
+    const issue = service.getIssue(req.params.id);
+    return res.status(200).json({ ...issue });
+  } catch (error) {
+    return res.status(error.status).json({ error: error.message });
+  }
 })
 
 app.post('/issues/', function (req, res) {
-  res.send('Hello World');
+  const { title, description } = service.getIssue(req.body);
+  const issue = service.createIssue(title, description);
+  return res.status(201).json({ ...issue });
 });
 
 app.put('/issues/:id', function (req, res) {
-  res.send('Hello World');
+  const { id } = req.params;
+  const { title, description } = req.body;
+  
+  try {
+    const issue = service.updateIssue(id, title, description);
+    return res.status(200).json({...issue });
+  } catch (error) {
+    return res.status(error.status).json({ error: error.message });
+  }
 });
 
 app.delete('/issues/:id', function (req, res) {
-  res.send('Hello World');
+  const { id } = req.params;
+  
+  try {
+    service.deleteIssue(id);
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(error.status).json({ error: error.message });
+  }
 });
 
 app.use((err, req, res, next) => {
