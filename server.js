5cba6f86a4128c4a04d8cebf85529545eaa14d87
nkinyua-mufin <n.kinyua@mufin.co.kr>
2024-09-07 11:34:51 +0300

    rename endpoints
    Signed-off-by: nkinyua-mufin <n.kinyua@mufin.co.kr>

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('issues/:id', function (req, res) {
+app.get('/issues/:id', function (req, res) {
   res.send('Hello World');
 })
 
-app.post('issues/', function (req, res) {
+app.post('/issues/', function (req, res) {
   res.send('Hello World');
 });
 
-app.put('issues/:id', function (req, res) {
+app.put('/issues/:id', function (req, res) {
   res.send('Hello World');
 });
 
-app.delete('issues/:id', function (req, res) {
+app.delete('/issues/:id', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
