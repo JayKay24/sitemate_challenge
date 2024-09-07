0948568aff85251ff65e2cfa6b298fc125dcba7b
nkinyua-mufin <n.kinyua@mufin.co.kr>
2024-09-07 11:53:46 +0300

    implement error-handling middleware
    Signed-off-by: nkinyua-mufin <n.kinyua@mufin.co.kr>

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.delete('/issues/:id', function (req, res) {
   res.send('Hello World');
 });
 
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+
+  return res.status(status).json({ error: message });
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
